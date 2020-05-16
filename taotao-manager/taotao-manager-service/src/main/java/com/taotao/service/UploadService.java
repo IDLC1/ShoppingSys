56738747589e83404fbdc342514f1a22469f27c7
licyafighting@gmail.com <licyafighting@gmail.com>
2020-05-16 21:37:37 +0800

    update .gitignore

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/UploadService.java
+++
@@ -1,8 +0,0 @@
-package com.taotao.service;
-
-import com.taotao.common.pojo.PictureResult;
-import org.springframework.web.multipart.MultipartFile;
-public interface UploadService {
-
-    PictureResult uploadFile(MultipartFile uploadFile);
-}
